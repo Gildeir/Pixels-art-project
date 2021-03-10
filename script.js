17a0d1c2e0e1207dea2840c97b0df24f30cef8be
Gildeir <gildeirlopes@gmail.com>
2021-03-10 18:00:47 -0300

    Tentando corrigir eventListener

--- script.js
+++ script.js
@@ -16,8 +16,21 @@ function createPixelBoard() {
 
 const arrPixel = document.querySelectorAll('.pixel');
 const setColor = ['black', 'red', 'green', 'blue'];
-for (let index = 0; index < document.querySelectorAll('.pixel').length; index += 1) {
-  document.querySelectorAll('.pixel').style.backgroundColor[index] = setColor[index];
+for (let index = 0; index < setColor.length; index += 1) {
+  arrPixel[index].style.backgroundColor[index] = setColor[index];
 }
 setColor[0].classList.add('selected');
-sessionStorage.setItem('setColor',setColor[0].style.backgroundColor);
+sessionStorage.setItem('setColor', setColor[0].style.backgroundColor);
+for (let indexB = 0; indexB < setColor.length; indexB += 1) {
+  document.querySelectorAll('.color')[indexB].addEventListener('click', () => {
+    document.querySelector('.selected').classList.remove('selected');
+    document.querySelectorAll('.color')[indexB].classList.add('selected');
+    sessionStorage.setItem('color', document.querySelectorAll('.color')[indexB].style.backgroundColor);
+  });
+}
+
+for (let index = 0; index < document.querySelectorAll('.pixel').length; index += 1) {
+  pixel[index].addEventListener('click', () => {
+    pixel[index].style.backgroundColor = sessionStorage.color;
+  });
+}
